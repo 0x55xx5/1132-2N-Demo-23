45f0041c931f3f385144465b1fd9fec1087dcd9d
Your Name <you@example.com>
2025-02-19 21:04:27 +0800

    w03

---
+++ 1132_w1_starter/starter/components/Navbar_xx.jsx
@@ -0,0 +1,10 @@
+import { links } from '../data';
+
+const Navbar_xx = () => {
+  return (
+    <nav>
+      <h1>Navbar_xx</h1>
+    </nav>
+  );
+};
+export default Navbar_xx;
---
+++ w01_tailwind/src/components/Navbar_xx.jsx
@@ -0,0 +1,35 @@
+import { links } from '../data';
+
+const Navbar_xx = () => {
+  return (
+    <nav className='bg-emerald-100'>
+      <div
+        className='my-btn
+        mx-auto flex flex-col 
+        sm:flex-row  
+        sm:items-center
+        sm:gap-x-16'
+      >
+        <h2 className='text-3xl font-bold '>
+          dev<span className='text-emerald-600'>My</span>
+        </h2>
+        <div className='flex gap-x-3'>
+          {links.map((link) => {
+            const { id, href, text } = link;
+            return (
+              <a
+                key={id}
+                href={href}
+                className='capitalize text-lg hover:text-emerald-600
+              tracking-wide duration-200'
+              >
+                {text}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar_xx;
