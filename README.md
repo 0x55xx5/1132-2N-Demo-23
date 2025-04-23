3bd6b0939746367ca5af2688476bbc5b099d9962
05235 <your_email@abc.example>
2025-04-23 21:03:16 +0800

    W10-P2: Prostore root layout and wrapper utility

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 1132-2N-Demo-23
 
 git log --pretty=format:"%h%x09%an%x09%ad%x09%s" --after="2025-02-18"
+
+
+npx create-next-app@latest
---
+++ w10/prostore/app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from '@/components/ui/button'
+import { Ghost } from 'lucide-react'
+
+const HomeIndex_23 = () => {
+  return (
+    <div>
+      
+      <Button variant='ghost' >YU 912410023 ghost</Button>
+      <Button variant='outline' >YU 912410023 outline</Button>
+    </div>
+  )
+}
+
+export default HomeIndex_23
