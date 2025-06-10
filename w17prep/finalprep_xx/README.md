56f26dc00a71ad1d420bf0f23e7f8781b0faed10
05235 <your_email@abc.example>
2025-06-11 00:37:27 +0800

    add prep

---
+++ w17prep/finalprep_xx/README.md
@@ -0,0 +1,10 @@
+```
+$ npm i prisma --save-dep
+
+# create schema.prisma
+$ npx prisma init
+
+$ npx prisma studio
+
+
+```
---
+++ w17prep/finalprep_xx/components/shared/header_xx/index.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { APP_NAME } from '@/lib/constants';
+import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Modetoggle_xx from './mode-toggle_xx';
+import Menu_xx from './menu_xx';
+
+const Header_xx = () => {
+  return (
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between'>
+        <div className='flex-start'>
+          <Link href='/'>
+            <Image
+              src='/images/logo.svg'
+              height={48}
+              width={48}
+              priority={true}
+              alt={`${APP_NAME} logo`}
+            />
+          </Link>
+        </div>
+        <Menu_xx />
+        {/* <div className='flex justify-end gap-3'>
+          <Modetoggle_xx />
+          <Button asChild variant='outline'>
+            <Link href='/cart_xx'>
+              <ShoppingCart /> Cart_xx
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link href='/sign-in'>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div> */}
+      </div>
+    </header>
+  );
+};
+
+export default Header_xx;
